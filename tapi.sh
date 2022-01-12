d67734a3481ae2db71f955dc1ae24ee442f37216
Kuohao-wu <www.guohao5789@gmail.com>
2022-01-12 21:22:07 +0800

    修复获取配置文件路径错误问题

--- tapi.sh
+++ tapi.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 . /Users/kuohao/mybash/tapi/config.sh
 header='Content-type':'application/x-www-form-urlencoded'
+
+
+###
+#   使用该脚本获取执行文件路径，避免软连接获取错误
+##
+SOURCE="$0"
+# resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE"  ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+    SOURCE="$(readlink "$SOURCE")"
+    # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    [[ $SOURCE != /*  ]] && SOURCE="$DIR/$SOURCE"
+done
+
+# 获取脚本文件路径
+DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+
+
 case $1 in
   -g)
     curl -s $requestUrl$2 | json
@@ -18,5 +36,5 @@ case $1 in
     echo $requestUrl
     ;;
   config)
-    echo "requestUrl=$2" > "$(pwd)/tapi/config.sh"
+    echo "requestUrl=$2" > "${DIR}/config.sh"
   esac
